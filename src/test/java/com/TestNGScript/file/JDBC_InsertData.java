47867474ee7a2392659a54eb16232fe935ea3bf4
chandan kumar <chandan.kumar@netsutra.com>
2019-10-15 12:37:01 +0530

    JDBC Database Testing

---
+++ src/test/java/com/TestNGScript/file/JDBC_InsertData.java
@@ -0,0 +1,40 @@
+package com.TestNGScript.file;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.testng.annotations.Test;
+
+public class JDBC_InsertData {
+
+	@Test
+
+	public void testPostgresDB() throws ClassNotFoundException, SQLException {
+
+		Class.forName("org.postgresql.Driver");
+
+		System.out.println("Database Driver Loaded");
+
+		Connection dbconnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/chandan", "postgres",
+				"omsai11ram");
+
+		System.out.println("Connected to PostGres SQL");
+
+		Statement stmnt = dbconnection.createStatement();
+
+		String query="insert into users values (7, 'Mi','kuvrr','Mi@yopmail.com', 5435465632, 12345678, 'sk')";
+		
+		stmnt.executeUpdate(query);
+
+		System.out.println("Insert Data done successfull verify DB");
+
+		// Close DB Connection
+		dbconnection.close();
+		System.out.println("DataBase Connection closed");
+
+	}
+
+}
---
+++ src/test/java/com/TestNGScript/file/JDBC_UpdateData.java
@@ -0,0 +1,43 @@
+package com.TestNGScript.file;
+
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import java.sql.Statement;
+	import org.testng.annotations.Test;
+
+	public class JDBC_UpdateData {
+
+		@Test
+
+		public void testPostgresDB() throws ClassNotFoundException, SQLException {
+
+			Class.forName("org.postgresql.Driver");
+
+			System.out.println("Database Driver Loaded");
+
+			Connection dbconnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/chandan", "postgres",
+					"omsai11ram");
+
+			System.out.println("Connected to PostGres SQL");
+
+			Statement stmnt = dbconnection.createStatement();
+			
+			String query= "update users set first_name='iphone' where id=3";
+			
+			stmnt.executeUpdate(query);
+
+			System.out.println("Update table done successfull verify DB");
+
+			// Close DB Connection
+			dbconnection.close();
+			System.out.println("DataBase Connection closed");
+
+		}
+
+	}
+
+
+
