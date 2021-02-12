0fa279387e428fa1f1d78e9cce2f9673f95997b7
chandan kumar <chandan.kumar@netsutra.com>
2021-02-12 12:19:57 +0530

    cu

--- src/test/java/com/TestNGScript/file/SK_ThermalCameraStatusCode.java
+++ src/test/java/com/TestNGScript/file/SK_ThermalCameraStatusCode.java
@@ -4,13 +4,16 @@ import org.testng.annotations.Test;
 import com.Commonutills.file.ApiPayLoad;
 import io.restassured.RestAssured;
 import io.restassured.RestAssured.*;
-
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.concurrent.TimeUnit;
+
 import org.json.simple.JSONObject;
 
-public class SK_ThermalCameraAPI extends ApiPayLoad {
+public class SK_ThermalCameraStatusCode extends ApiPayLoad {
 
 	@Test
 
@@ -19,16 +22,21 @@ public class SK_ThermalCameraAPI extends ApiPayLoad {
 		JSONObject Postrequest = new JSONObject(PostRequestBody());
 		System.out.println(Postrequest.toJSONString());
 
-		given()
+		Response res=given()
 		.header("Content-Type", "application/json")
 		.header("Authorization", "Basic Tk9QRFFJSHJieHFUanVOOk1ndEYxeElHY09WZUdvZzVzWnJoa3BmeVNsRG01WFpmekNlYkRMOVJO")
 		.body(Postrequest.toJSONString())
 		.when()
 		.post("https://safety-test.kuvrr.com/api/v1/callbacks/FaceMaskThermalScanRecord/")
 		.then()
-		.statusCode(200)
-		.assertThat()
-		.statusCode(200);
+		.extract()
+		.response();
+		
+		System.out.println("Post Response Status Code is :"+ res.statusCode());
+		System.out.println("Post Response headers are :"+ res.headers());
+		System.out.println("Post Response body are :"+ res.body());
+		System.out.println("Post Response Time is :"+ res.timeIn(TimeUnit.MILLISECONDS)+ " milliseconds");
+	
 		
 	
 
